88960813312b3a951bd0b40c7dadddfdc3dba51a
Josh LaRose <46906264+joshlarose12@users.noreply.github.com>
2021-01-23 19:30:34 -0500

    Module 7

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function CourseList(props) {
   return (
@@ -14,7 +16,9 @@ function CourseList(props) {
         {props.courses.map((course) => {
           return (
             <tr key={course.id}>
-              <td>{course.title}</td>
+              <td>
+                <Link to={"/course/" + course.slug}> {course.title}</Link>
+              </td>
               <td>{course.authorId}</td>
               <td>{course.category}</td>
             </tr>
@@ -24,5 +28,16 @@ function CourseList(props) {
     </table>
   );
 }
+
+CourseList.propTypes = {
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default CourseList;
--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 import CourseList from "./CourseList";
+import { Link } from "react-router-dom";
 
 function CoursesPage() {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    getCourses().then((_courses) => setCourses(courses));
-  }, [courses]);
+    getCourses().then((_courses) => setCourses(_courses));
+  }, []);
 
   return (
     <>
